9458c7cd1ca0b5373c044c89ed5d85828f55cb1d
ironpark <cjfdhksaos@gmail.com>
2024-08-25 13:40:26 +0900

    refactor: Refactor cmd package structure

--- cmd/cmd.go
+++ cmd/cmd.go
@@ -1,8 +1,17 @@
 package cmd
 
-import "github.com/urfave/cli/v2"
+import (
+	"github.com/ironpark/zapp/cmd/dmg"
+	"github.com/ironpark/zapp/cmd/pkg"
+	"github.com/ironpark/zapp/cmd/sign"
+	"github.com/urfave/cli/v2"
+)
 
-var commands []*cli.Command
+var commands = []*cli.Command{
+	dmg.Command,
+	pkg.Command,
+	sign.Command,
+}
 
 func GetCommands() []*cli.Command {
 	return commands
